df28e38b470393ac7168d4de4f0cda822a7ba2d6
xArbisRox <fabiangeorgfrank@gmail.com>
2024-06-01 17:26:20 +0200

    wrap Expo Tab navigation into ReactNativePaper Bottom Navigation Bar style

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,6 +1,8 @@
 import { Tabs } from "expo-router";
 import { FontAwesome } from "@expo/vector-icons";
 import { StyleSheet } from "react-native";
+import { CommonActions } from "@react-navigation/native";
+import { BottomNavigation } from "react-native-paper";
 
 const style = StyleSheet.create({
   tabBarLabel: {
@@ -9,7 +11,50 @@ const style = StyleSheet.create({
 });
 export default function MyTabs() {
   return (
-    <Tabs>
+    <Tabs
+      // below tabBar is simply copy pasted boilerplate from the docs at https://callstack.github.io/react-native-paper/docs/components/BottomNavigation/BottomNavigationBar
+      tabBar={({ navigation, state, descriptors, insets }) => (
+        <BottomNavigation.Bar
+          navigationState={state}
+          safeAreaInsets={insets}
+          onTabPress={({ route, preventDefault }) => {
+            const event = navigation.emit({
+              type: "tabPress",
+              target: route.key,
+              canPreventDefault: true,
+            });
+
+            if (event.defaultPrevented) {
+              preventDefault();
+            } else {
+              navigation.dispatch({
+                ...CommonActions.navigate(route.name, route.params),
+                target: state.key,
+              });
+            }
+          }}
+          renderIcon={({ route, focused, color }) => {
+            const { options } = descriptors[route.key];
+            if (options.tabBarIcon) {
+              return options.tabBarIcon({ focused, color, size: 24 });
+            }
+
+            return null;
+          }}
+          getLabelText={({ route }) => {
+            const { options } = descriptors[route.key];
+            const label =
+              options.tabBarLabel !== undefined
+                ? options.tabBarLabel
+                : options.title !== undefined
+                  ? options.title
+                  : route.title;
+
+            return label;
+          }}
+        />
+      )}
+    >
       <Tabs.Screen
         name="index"
         options={{
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,12 @@
 import { Stack } from "expo-router";
+import { PaperProvider } from "react-native-paper";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <PaperProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </PaperProvider>
   );
 }
