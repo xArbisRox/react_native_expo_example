dd07a106096303f8b59342d21000925527ade168
xArbisRox <fabiangeorgfrank@gmail.com>
2024-06-02 15:41:18 +0200

    Put GamesDataTable in separate component

--- app/(tabs)/team/index.jsx
+++ app/(tabs)/team/index.jsx
@@ -1,10 +1,11 @@
 import * as React from "react";
 import { useState } from "react";
 import { StyleSheet, View } from "react-native";
-import { DataTable, useTheme } from "react-native-paper";
+import { useTheme } from "react-native-paper";
 import Background from "../../../components/custom/Gradient";
 import CustomDropdown from "../../../components/custom/Dropdown";
 import CustomCheckbox from "../../../components/custom/Checkbox";
+import GamesDataTable from "../../../components/custom/team/GamesDataTable";
 import baseStyles from "@/components/custom/Style";
 
 const GamesPage = () => {
@@ -26,11 +27,7 @@ const GamesPage = () => {
       flexDirection: "row",
       justifyContent: "space-evenly",
       alignItems: "center",
-      backgroundColor: theme.colors.surfaceVariant,
-    },
-    datatable: {
-      height: "70%",
-      backgroundColor: theme.colors.surfaceVariant,
+      backgroundColor: theme.colors.surface,
     },
   });
 
@@ -55,47 +52,6 @@ const GamesPage = () => {
   // Checkbox section
   const [playChecked, setPlayChecked] = useState(false);
   const [multipleChecked, setMultipleChecked] = useState(false);
-
-  // Datatable section
-  const [page, setPage] = React.useState(0);
-  const [numberOfItemsPerPageList] = React.useState([2, 3, 4]);
-  const [itemsPerPage, onItemsPerPageChange] = React.useState(
-    numberOfItemsPerPageList[0],
-  );
-
-  const [items] = React.useState([
-    {
-      key: 1,
-      name: "Cupcake",
-      calories: 356,
-      fat: 16,
-    },
-    {
-      key: 2,
-      name: "Eclair",
-      calories: 262,
-      fat: 16,
-    },
-    {
-      key: 3,
-      name: "Frozen yogurt",
-      calories: 159,
-      fat: 6,
-    },
-    {
-      key: 4,
-      name: "Gingerbread",
-      calories: 305,
-      fat: 3.7,
-    },
-  ]);
-
-  const from = page * itemsPerPage;
-  const to = Math.min((page + 1) * itemsPerPage, items.length);
-
-  React.useEffect(() => {
-    setPage(0);
-  }, [itemsPerPage]);
 
   return (
     <View style={[baseStyles.container, { justifyContent: "start" }]}>
@@ -125,33 +81,7 @@ const GamesPage = () => {
           />
         </View>
       </View>
-      <DataTable style={styles.datatable}>
-        <DataTable.Header>
-          <DataTable.Title>Dessert</DataTable.Title>
-          <DataTable.Title numeric>Calories</DataTable.Title>
-          <DataTable.Title numeric>Fat</DataTable.Title>
-        </DataTable.Header>
-
-        {items.slice(from, to).map((item) => (
-          <DataTable.Row key={item.key}>
-            <DataTable.Cell>{item.name}</DataTable.Cell>
-            <DataTable.Cell numeric>{item.calories}</DataTable.Cell>
-            <DataTable.Cell numeric>{item.fat}</DataTable.Cell>
-          </DataTable.Row>
-        ))}
-
-        <DataTable.Pagination
-          page={page}
-          numberOfPages={Math.ceil(items.length / itemsPerPage)}
-          onPageChange={(page) => setPage(page)}
-          label={`${from + 1}-${to} of ${items.length}`}
-          numberOfItemsPerPageList={numberOfItemsPerPageList}
-          numberOfItemsPerPage={itemsPerPage}
-          onItemsPerPageChange={onItemsPerPageChange}
-          showFastPaginationControls
-          selectPageDropdownLabel={"Rows per page"}
-        />
-      </DataTable>
+      <GamesDataTable />
     </View>
   );
 };
